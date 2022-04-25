9b1b37c829adc80fe02ebe87c1ac8604648cdc30
Hmuriy01 <99268976+Hmuriy01@users.noreply.github.com>
2022-04-25 15:54:33 +0300

    Update Duck5.java
    Сделал    Autowired поле

--- PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Duck5.java
+++ PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Duck5.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-
-    private final Egg6 egg6;
+    
     @Autowired
+    private final Egg6 egg6;
+
     public Duck5(Egg6 egg6) {
         this.egg6 = egg6;
     }
