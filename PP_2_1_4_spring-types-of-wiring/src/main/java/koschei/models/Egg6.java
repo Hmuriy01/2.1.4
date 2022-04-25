9d9e40ebf662c7fef18d1f8accd36e476fcabbe7
Hmuriy01 <99268976+Hmuriy01@users.noreply.github.com>
2022-04-25 15:55:15 +0300

    Update Egg6.java
    Добавил Setter, сделал его Autowired

--- PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Egg6.java
+++ PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Egg6.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Egg6 {
 
-    private final Needle7 needle7;
+    private Needle7 needle7;
 
-    @Autowired
     public Egg6 (Needle7 needle7) {
+        this.needle7 = needle7;
+    }
+    @Autowired
+    public void setNeedle(Needle7 needle7) {
         this.needle7 = needle7;
     }
 
